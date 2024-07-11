141bb5c546f8b650f00790ea5c891fc92066929f
Maud Gautier <maud.j.gautier@gmail.com>
2024-07-11 10:11:34 -0600

    feat(#3e): initialize 3e challenge from #3c

---
+++ 3e-efficient-broadcast-II/test.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Build
+go build -o bin
+
+# With nemesis
+$MAELSTROM test -w broadcast --bin bin --node-count 25 --time-limit 20 --rate 100 --latency 100 --nemesis partition > /dev/null 2>&1
+
+echo -e "\n-- VALIDITY RESULTS WITH NEMESIS:"
+tail -n1 ./store/latest/jepsen.log
+
+
+# Without nemesis - checking performance
+$MAELSTROM test -w broadcast --bin bin --node-count 25 --time-limit 20 --rate 100 --latency 100 > /dev/null 2>&1
+
+echo -e "\n-- VALIDITY RESULTS:"
+tail -n1 ./store/latest/jepsen.log
+echo -e "\n-- MESSAGE PER OPS RESULTS:"
+grep -A 9 ".net" ./store/latest/results.edn
+echo -e "\n-- LATENCY RESULTS:"
+grep -A 4 ":stable-latencies" ./store/latest/results.edn
+
+echo -e "\n-- EXPECTATIONS:"
+echo -e "   - msgs-per-ops   < 20"
+echo -e "   - median latency < 1,000ms"
+echo -e "   - max latency    < 2,000ms"
+
+
