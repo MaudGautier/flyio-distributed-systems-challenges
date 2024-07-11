d36562d8720194d4c3a9b83897eaf339f9dcf997
Maud Gautier <maud.j.gautier@gmail.com>
2024-07-11 10:05:55 -0600

    refactor(#3c): move challenge to own folder

---
+++ 3c-fault-tolerant-broadcast/test.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+go build -o bin
+$MAELSTROM test -w broadcast --bin bin --node-count 5 --time-limit 20 --rate 10 --nemesis partition > /dev/null 2>&1
+
+echo -e "\n-- VALIDITY RESULTS:"
+tail -n1 ./store/latest/jepsen.log
