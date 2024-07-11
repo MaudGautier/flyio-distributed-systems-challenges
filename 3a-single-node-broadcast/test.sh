4cf4cea1dbd6257ff01948da92398a3e8fc67fee
Maud Gautier <maud.j.gautier@gmail.com>
2024-07-11 09:49:12 -0600

    refactor(#3a): move challenge to own folder

---
+++ 3a-single-node-broadcast/test.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+go build -o bin
+$MAELSTROM test -w broadcast --bin bin --node-count 1 --time-limit 20 --rate 10 > /dev/null 2>&1
+
+echo "-- VALIDITY RESULTS:"
+tail -n1 ./store/latest/jepsen.log
